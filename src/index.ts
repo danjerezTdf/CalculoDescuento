6585f2318e85fc7a8a3eccf8e00ff1ecb62e5398
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-25 20:05:13 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let precioProducto: number = 450.5;
+let porcentajeDescuento: number = 0.1;
+let decuento: number = precioProducto * porcentajeDescuento;
+let precioFinal: number = precioProducto - decuento;
+console.log(`El precio Final con descuento: `, precioFinal);
